15c7d0ac38768046b93623b3593053037e8c8821
Petrov-Stormwing <selina.tsukiomi@gmail.com>
2024-05-15 16:10:08 +0200

    implemented attack function and cut out the reservation as its wasn't functional

--- role.Claimer.js
+++ role.Claimer.js
@@ -1,23 +1,25 @@
-require('ResourcesUtility');
-
 let roleClaimer = {
 
     /** @param {Creep} creep **/
     run: function (creep) {
-        creep.memory.targetRoom='W59S5';
-        if (creep.room.name !== creep.memory.targetRoom) {
-            creep.moveTo(new RoomPosition(28, 14, creep.memory.targetRoom), {visualizePathStyle: {stroke: '#ffaa00'}});
+        creep.memory.targetRoom = 'W59S3';
+        // if (creep.room.name !== creep.memory.targetRoom) {
+        let roomPosition=new RoomPosition(26, 35, creep.memory.targetRoom);
+        creep.moveTo(roomPosition);
+        if (creep.room.controller.owner) {
+            creep.attackController(Game.getObjectById("5bbca9dd9099fc012e630402"))
         } else {
-            if (!creep.room.controller.reservation) {
-                let reservation = ReserveController(creep);
-                if (reservation === 0) {
-                    console.log("Controller in " + creep.memory.targetRoom + " Successfully Reserved",);
-                    console.log(`Controller Status: ${Game.rooms['W59S5'].controller.reservation}`)
-                }
-            } else {
-                ClaimController(creep);
-            }
+            ClaimController(creep);
         }
+
+        //TODO: Define a Reservation Strategy
+        // if (!creep.room.controller.reservation) {
+        //     let reservation = ReserveController(creep);
+        //     if (reservation === 0) {
+        //         console.log("Controller in " + creep.memory.targetRoom + " Successfully Reserved",);
+        //         console.log(`Controller Status: ${Game.rooms['W59S3'].controller.reservation}`)
+        //     }
+        // }
     }
 };
 
